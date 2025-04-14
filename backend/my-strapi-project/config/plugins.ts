bf2d15c1018dbab9788ae76eb896a398781c9981
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-04-14 12:16:25 +0530

    added phone and type to registration

--- backend/my-strapi-project/config/plugins.ts
+++ backend/my-strapi-project/config/plugins.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+export default ({ env }) => ({
+    "users-permissions": {
+    config: {
+      register: {
+        allowedFields: ["phone", "type"],
+      },
+    },
+  }
+});
